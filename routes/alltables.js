42fbd343db1ac79b2eaec6058c8fba1a9be0f6a1
Gr3enP0ison <wewantthejoker@gmail.com>
2018-04-20 10:51:02 +0200

    Initial Commit

---
+++ routes/alltables.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var DB = require('./db.js');
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    DB.getAllTables(function (err, results) {
+        if (err) {
+            console.log('Scotty, we have an error!!');
+            return;
+        }
+        if (results != null){
+            console.log(results);
+            res.send(results);
+        }
+    });
+});
+
+module.exports = router;
